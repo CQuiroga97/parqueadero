59c784a838e6c908751262af57e068a9538d634d
CQuiroga97 <quiroga.cristian97@hotmail.com>
2019-09-30 15:11:13 -0500

    cambios

--- conexion.php
+++ conexion.php
@@ -1,8 +1,9 @@
 <?php
 $host = "localhost";
+$pass = "";
 $db = "parqueadero";
 $dns = "mysql:host=$host;dbname=$db";
-$con= new PDO($dns, $user, $pass);
+$con= new PDO($dns, "root", $pass);
 
 
 ?>
---
+++ plantilla.php
@@ -0,0 +1,5 @@
+<html>
+<head>
+<link href="css" rel="stylesheet" type="text/css">
+</head>
+<body>
